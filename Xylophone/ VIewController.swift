a65a92970dc892217279c84a394da7625fe06d60
hersusanto <herisusanto.id@gmail.com>
2019-09-13 20:12:29 +0700

    Try to access AVFoundation library by creating simple app

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1","note2","note3","note4","note5","note6","note7",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +22,22 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        playSound(tag:sender.tag)
         
+        
+    }
+    func playSound (tag:Int){
+        
+        let soundUrl = Bundle.main.url(forResource: soundArray[tag-1], withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
         
     }
     
